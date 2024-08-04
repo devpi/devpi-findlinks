b2da448a839778926b00a69b6da250cc785e211f
Florian Schulze <mail@florian-schulze.net>
2024-08-04 07:58:10 +0200

    Fix deprecation warning for 'transaction'.

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,11 +17,17 @@ else:
     pytest_plugins = ["pytest_devpi_server", "test_devpi_server.plugin"]
 
 
+def write_transaction(keyfs):
+    if hasattr(keyfs, 'write_transaction'):
+        return keyfs.write_transaction()
+    return keyfs.transaction(write=True)
+
+
 @pytest.fixture
 def xom(request, makexom):
     import devpi_findlinks.main
     xom = makexom(plugins=[(devpi_findlinks.main, None)])
     from devpi_server.main import set_default_indexes
-    with xom.keyfs.transaction(write=True):
+    with write_transaction(xom.keyfs):
         set_default_indexes(xom.model)
     return xom
